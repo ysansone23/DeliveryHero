9194549f2525a8784b05d4305c52c9171374e564
ysansone <yamila.sansone@mercadolibre.com>
2019-08-08 23:08:43 -0300

    Add splash activity

---
+++ DeliveryApp/src/main/java/com/app/delivery/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.app.delivery
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.delivery.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.splash_activity_layout)
+    }
+}
